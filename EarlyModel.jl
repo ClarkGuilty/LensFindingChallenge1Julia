74899719a8410145a70bd45840405452e0f95d3a
Javier Acevedo <ja.acevedo12@unianes.edu.co>
2021-07-04 01:35:22 -0500

    "Bigger model, VRAM problems but so far batch size can still be 30 or bigger."

--- EarlyModel.jl
+++ EarlyModel.jl
@@ -85,33 +85,45 @@ end
 function preprocess_labels_gpu!(labels)
   gpu(onehotbatch(labels,0:1))
 end
+
+function preprocess_labels!(labels)
+  onehotbatch(labels,0:1)
+end
 od(W,F,P=0,S=1) = 1 + Int((W-F+2*P)/S)
 
 #Using LASTRO architecture
 ##
-mrelu(x) = (sqrt(2*π) * max(zero(x),x)-1)/sqrt(π-1)
+oftf(x, y) = oftype(float(x), y)
+function mrelu(x) 
+  a = oftf(x,2*π)
+  b = oftf(x,sqrt(π-1))
+  (a * max(zero(x),x)-1)/b
+end
 
-l1c1 =  Conv((4, 4), 1=>8, pad=(0,0), mrelu) # 98,98,8 -> 128, 136
-l1c2 =  Conv((3, 3), 8=>8, pad=(0,0), mrelu) # 96,96,8 -> 576, 584
-l1p1 = MaxPool((2,2),pad=0, stride=2)       # 48,48,8
-l1b1 = Flux.BatchNorm(8)
+l1c1 =  Conv((4, 4), 1=>16, pad=(0,0), mrelu) # 98,98,16 -> 256, 272
+l1c2 =  Conv((3, 3), 16=>16, pad=(0,0), mrelu) # 96,96,16 -> 2304, 2320
+l1p1 = MaxPool((2,2),pad=0, stride=2)       # 48,48,16
+l1b1 = Flux.BatchNorm(16,affine=true)       # 48,48,16 -> 32
 l1 = Chain(l1c1,l1c2,l1p1,l1b1)
 #
-l2c1 =  Conv((3, 3), 8=>16, pad=(0,0), mrelu)# 46,46,16 -> 1152, 1168
+l2c1 =  Conv((3, 3), 16=>16, pad=(0,0), mrelu)# 46,46,16 -> 2304, 2320
 l2c2 =  Conv((3, 3), 16=>16, pad=(0,0), mrelu)#44,44,16 -> 2304, 2320
 l2p1 = MaxPool((2,2),pad=0, stride=2)        #22,22,16
-l2b1 = Flux.BatchNorm(16)
+l2b1 = Flux.BatchNorm(16,affine=true)        #22,22,16 -> 32
+
 l2 = Chain(l2c1,l2c2,l2p1,l2b1)
 #
 l3c1 =  Conv((3, 3), 16=>16, pad=(0,0), mrelu)#20,20,16 -> 2304, 2320
 l3c2 =  Conv((3, 3), 16=>16, pad=(0,0), mrelu)#18,18,16 -> 2304, 2320
 l3p1 = MaxPool((2,2),pad=0, stride=2)        #9, 9, 16
-l3b1 = Flux.BatchNorm(16)
-l3 = Chain(l3c1,l3c2,l3p1,l3b1)
+l3b1 = Flux.BatchNorm(16,affine=true)        #9, 9, 16 -> 32
+l3b2 = Flux.Dropout(0.5,dims=3)
+l3 = Chain(l3c1,l3c2,l3p1,l3b1,l3b2)
 #
 l4c1 =  Conv((3, 3), 16=>32, pad=(0,0), mrelu)#7, 7, 32 -> 4608, 4640
+l4d1 = Flux.Dropout(0.5,dims=3)
 l4c2 =  Conv((3, 3), 32=>32, pad=(0,0), mrelu)#5, 5, 32 -> 9216, 9248
-l4b1 = Flux.BatchNorm(32)
+l4b1 = Flux.BatchNorm(32,affine=true)        #5, 5, 32 -> 64
 l4 = Chain(l4c1,l4c2,l4b1)
 #
 lf0 = Flux.flatten
@@ -119,59 +131,103 @@ lf1 = Dense(800,2) #-> 1602
 #lf2 = Dense(512,2)
 l5 = Chain(lf0,lf1)
 
-f = gpu(Chain(l1c1,l1c2,l1p1,l1b1,l2c1,l2c2,l2p1,l2b1,l3c1,l3c2,l3p1,l3b1,l4c1,l4c2,l4b1,lf0,lf1)) # 24338
+f = gpu(Chain(l1c1,l1c2,l1p1,l1b1,l2c1,l2c2,l2p1,l2b1,l3c1,l3c2,l3p1,l3b1,l3b2,l4c1,l4d1,l4c2,l4b1,lf0,lf1)) # 27458
+#f0 = gpu(Chain(l1c1,l1c2,l1p1,l1b1,l2c1,l2c2,l2p1,l2b1,l3c1,l3c2,l3p1,l3b1,l3b2,l4c1,l4d1,l4c2,l4b1)) # 24338
 ##
 parameters = Flux.params(f)
-loss_f(x,y) = logitcrossentropy(f(x),y)
-accuracy(x,y) = mean(onecold(cpu(f(x))) .== onecold(cpu(y)))
+loss_f(x,y) = logitcrossentropy(trainmode!(f,true)(x),y)
+using Flux: OneHotMatrix
+compare(y::OneHotMatrix, y′) = maximum(y′, dims = 1) .== maximum(y .* y′, dims = 1)
+accuracy(x, y::OneHotMatrix,testmode=true) = mean(compare(y, testmode!(f,testmode)(x)))
 
+function loss_and_accuracy(x,y,testmode=true)
+  ŷ = testmode!(f,testmode)(x)
+  logitcrossentropy(ŷ,y), mean(compare(y, ŷ))
+end
+
+function loss_and_accuracy((x,y),batchsize::Int64)
+  loss = 0; accuracy = 0; i = 0
+  dataloader = Flux.Data.DataLoader((x, y), batchsize=batchsize, shuffle=false)
+  for (a,b) in dataloader
+    a,b = gpu(a), gpu(b)
+    ŷ = testmode!(f,true)(a)
+    loss += logitcrossentropy(ŷ,b)
+    accuracy += mean(compare(b, ŷ))
+    i += 1
+    #CUDA.unsafe_free!(a)
+    a = nothing
+    b = nothing
+  end
+  #dataloader = nothing
+  loss / i, accuracy / i, i
+end
 ##Load data
 
-
 #(train_images, train_labels) = getobs(trainData,1:nobs(trainData))
-(train_images, train_labels) = getobs(trainData,1:7)
+(train_images, train_labels) = getobs(trainData,1:100)
 (test_images, test_labels) = getobs(testData,1:nobs(testData))
 
 train_images = preprocess_images_gpu!(train_images)
-test_images = preprocess_images_gpu!(test_images)
+test_images = preprocess_images!(test_images)
 train_labels = preprocess_labels_gpu!(train_labels)
-test_labels = preprocess_labels_gpu!(test_labels)
+test_labels = preprocess_labels!(test_labels)
+
+test_loader = Flux.Data.DataLoader((test_images, test_labels), batchsize=60, shuffle=false)
+#@benchmark CUDA.@sync loss_and_accuracy((test_images, test_labels), 800)
+loss_and_accuracy((test_images, test_labels), 400)
 #accuracy(train_images,train_labels)
-#accuracy(test_images,test_labels)
+a,b = loss_and_accuracy(test_images,test_labels,400)
 ##
-#loss_f(test_images,test_labels)
+
 
 
 
 ##Assumes test data is already in gpu.
-function my_train!(trainData::ImageDataset, testData, batches, nepochs, loss , opt, parameters,accuracy)
-  batchSize = floor(Int,nobs(trainData)/batches)
+"Trains model. trainData must be an ImageDataset, testData a gpu tuple, nbatches and nepochs are number of batches and epochs respectectively, loss is the loss function, opt and parameters are the optimizer and its parameters, and accuracy is the accuracy function."
+function my_train!(trainData::ImageDataset, testData, nbatches, nepochs, loss , opt, parameters,accuracy, history_loss, history_accuracy, iterations_so_far)
+  batchSize = floor(Int,nobs(trainData)/nbatches)
   index(index,batch,batchSize=batchSize) = index + (batch-1)*batchSize
   #evalcb() = @show(accuracy(images,labels))
   s = ParameterSchedulers.Stateful((Exp(λ = 5e-1, γ = 0.9)))
-  #println(batchSize,"-",index(1,1),"-",index(1,batches))
+  #println(batchSize,"-",index(1,1),"-",index(1,nbatches))
   #Serial and batchwise loading. Move to dataloaders when
-  p = Progress(nepochs*batches)
+  println("Training $nepochs epochs with $nbatches batches of size $batchSize.")
+  p = Progress(nepochs*nbatches)
   for j in 1:nepochs
-    for i in 1:batches
+    for i in 1:nbatches
       k = index(1,i)
       (train_images, train_labels) = getobs(trainData,k:k+batchSize)
       train_images = preprocess_images_gpu!(train_images)
       train_labels = preprocess_labels_gpu!(train_labels)
 
-      opt.eta = ParameterSchedulers.next!(s)
-      #i+j % 5 == 0 ? println(opt.eta,  " - ", accuracy(testData[1],testData[2])) : nothing
+      #opt.eta = ParameterSchedulers.next!(s)
+      #i % 5 == 0 ? push!(history_loss, accuracy(testData[1],testData[2])) : nothing
+      if i % 10 == 0
+        t_loss, t_accuracy, _ = loss_and_accuracy((testData[1],testData[2]),400)
+        push!(history_accuracy, t_accuracy)
+        push!(history_loss, t_loss)
+        println("Epoch $j, Batch $i, accuracy: $t_accuracy, loss: $t_loss")
+
+      end
       g = Flux.gradient(() -> loss(train_images, train_labels), parameters)
       Flux.update!(opt, parameters, g)
       ProgressMeter.next!(p)
+      iterations_so_far = iterations_so_far + 1
     end
   end
+  t_loss, t_accuracy, _ = loss_and_accuracy((testData[1],testData[2]),400)
+  println("Test accuracy: $t_accuracy, Test loss: $t_loss.")
 end
 #my_train!(trainData, (test_images, test_labels), 10, 1,
-#loss_f, Descent(),parameters,accuracy)
 
+history_loss = []
+history_accuracy = []
+##
+iterations = 0
 
-my_train!(trainData, (test_images, test_labels), 50, 2,
-loss_f, ADAM( ),parameters,accuracy)
+my_train!(trainData, (test_images, test_labels), 50, 2,loss_f,
+          ADAM(),parameters,accuracy,history_loss, history_accuracy,iterations)
+
 accuracy(test_images,test_labels)
-
+##
+plot(history_loss,label=:none); plot!(twinx(),history_accuracy, color = :orange, label=:none)
